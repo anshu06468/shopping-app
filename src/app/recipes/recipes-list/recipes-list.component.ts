02f23a6334291678656aa3a584b1e331ac1eaf83
anshu06468 <anshukumar.kumar447@gmail.com>
2020-03-08 17:41:05 +0530

    many small changes

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
+
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,13 +8,18 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelectd=new EventEmitter<Recipe>();
   recipes:Recipe[]=[
-    new Recipe("A test Recipe","Description",'https://lazizkhana.com/wp-content/uploads/2015/09/pani-puri.jpg')
+    new Recipe("Pani poori","Description",'https://lazizkhana.com/wp-content/uploads/2015/09/pani-puri.jpg'),
+    new Recipe("Samosa","Description",'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-500x500.jpg')
     
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelectd.emit(recipe);
   }
 
 }
